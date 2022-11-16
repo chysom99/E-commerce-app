3b053af17e76d65ec4ee3fb41cd5d26ee021429f
chisom <chisom.ofoedu@flutterwavego.com>
2022-11-16 05:35:25 +0100

    initial commit

--- src/api/middleware/controllers/book.js
+++ src/api/middleware/controllers/book.js
@@ -3,18 +3,28 @@ const { isValidObjectId } = require("../../../utils.js/validator");
 
 module.exports = {
   create: async (req, res) => {
-    const { title, description, subject, quantity, dimension, price, user } =
-      req.body;
+    const {
+      title,
+      description,
+      subject,
+      total_quantity,
+      dimension,
+      total_price,
+      authorInformation,
+    } = req.body;
 
-    if (!isValidObjectId(user))
-      return res.json({ message: "User is invalid " });
+    if (!isValidObjectId(authorInformation))
+      return res.json({ message: "authorInformation is invalid " });
     if (!title) return res.json({ message: "title is required" });
     if (!description) return res.json({ message: "description is required" });
     if (!subject) return res.json({ message: "subject is required" });
-    if (!quantity) return res.json({ message: "quantity is required" });
+    if (!total_quantity)
+      return res.json({ message: "total_quantity is required" });
     if (!dimension) return res.json({ message: "dimension is required" });
-    if (!price) return res.json({ message: "price is required" });
+    if (!total_price) return res.json({ message: "total_price is required" });
 
+    let quantity = { inStock: total_quantity, rentedout: 0 };
+    let price = { dailyRental: total_price, currency: "#" };
     let book = new Book({
       title,
       description,
@@ -22,10 +32,27 @@ module.exports = {
       price,
       quantity,
       dimension,
-      user,
+      authorInformation,
     });
 
     book = await book.save();
+    res.json(book);
+  },
+
+  //update
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { subject } = req.body;
+    if (!isValidObjectId(id))
+      return res.json({ message: "Book Id is invalid" });
+    if (!subject) return res.json({ message: "subject is required" });
+
+    let book = await Book.findById(id).exec();
+    if (!book) return res.status(400).json({ message: "Book not found" });
+    book.subject = subject;
+
+    book = await book.save();
+
     res.json(book);
   },
 
@@ -34,13 +61,28 @@ module.exports = {
     const { id } = req.params;
     if (!isValidObjectId(id))
       return res.json({ message: "Book id is invalid" });
-    const book = await Book.findByIdAndDelete(id).exec();
-    if (!book) return res.json({ messsage: "book not found" });
+    const book = await Book.findOneAndDelete(id).exec();
+    if (!book) return res.status(400).json({ messsage: "book not found" });
+    else return res.status(200).json({ messsage: "book deleted" });
   },
 
   //fetch all
   find: async (req, res) => {
     const book = await Book.find({});
-    res.json({ book });
+    res.status(200).json({ book });
+  },
+
+  //search
+  findAll: async (req, res) => {
+    const where = {};
+    if (req.query.filter) {
+      where.title = {
+        $regex: req.query.filter,
+        $options: "i",
+      };
+    }
+    const book = await Book.find(where).exec();
+
+    res.json(book);
   },
 };
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,8 +4,14 @@ const createUserController = require("../middleware/controllers/user");
 const { signup, login } = createUserController;
 const userAuth = require("../middleware/auth");
 const auth_token = require("../middleware/auth_token");
+const book = require("../middleware/controllers/book");
 
 router.post("/signup", userAuth.validateUser, signup);
 router.post("/login", login);
+router.post("/book", auth_token, book.create);
+router.put("/book/:id", auth_token, book.update);
+router.delete("/book/:id", auth_token, book.delete);
+router.get("/book", auth_token, book.find);
+router.get("/book/search", auth_token, book.findAll);
 
 module.exports = router;
