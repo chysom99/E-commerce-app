874b04909d9c0bc07c4f715fce4237ec1cbb3f01
chisom <chisom.ofoedu@flutterwavego.com>
2022-11-20 21:37:45 +0100

    added a validationerror for body response

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { ValidationError } = require("express-validation");
+
 const app = express();
 const routes = require("../src/api/routes/index");
 
@@ -6,8 +9,19 @@ require("../src/models/index");
 
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api/v1/", routes);
+
+app.use(function (err, req, res, next) {
+  console.log(err);
+  if (err instanceof ValidationError) {
+    return res
+      .status(err.statusCode)
+      .json(err.details.body.map((x) => x.message));
+  }
+  return res.status(500).json(err);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on " + (process.env.PORT || 3000));
