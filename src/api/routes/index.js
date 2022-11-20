b69021c0b78249a2c4c46084e2995df47f80305d
chisom <chisom.ofoedu@flutterwavego.com>
2022-11-20 21:45:01 +0100

    added bookvalidator to the route path

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,11 +5,23 @@ const { signup, login } = createUserController;
 const userAuth = require("../middleware/auth");
 const auth_token = require("../middleware/auth_token");
 const book = require("../middleware/controllers/book");
+const BookValidator = require("../../utils.js/book.validator");
+const { validate } = require("express-validation");
 
 router.post("/signup", userAuth.validateUser, signup);
 router.post("/login", login);
-router.post("/book", auth_token, book.create);
-router.put("/book/:id", auth_token, book.update);
+router.post(
+  "/book",
+  auth_token,
+  validate(BookValidator.createBookValidator),
+  book.create
+);
+router.put(
+  "/book/:id",
+  auth_token,
+  validate(BookValidator.updateBookValidator),
+  book.update
+);
 router.delete("/book/:id", auth_token, book.delete);
 router.get("/book", auth_token, book.find);
 router.get("/book/search", auth_token, book.findAll);
