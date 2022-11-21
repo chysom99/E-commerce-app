a71af895fe55fb9ae527448e4eda4f00d4230248
chisom <chisom.ofoedu@flutterwavego.com>
2022-11-21 10:30:36 +0100

    added a route path for create and update review

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,7 +5,9 @@ const { signup, login } = createUserController;
 const userAuth = require("../middleware/auth");
 const auth_token = require("../middleware/auth_token");
 const book = require("../middleware/controllers/book");
+const review = require("../middleware/controllers/review");
 const BookValidator = require("../../utils.js/book.validator");
+const ReviewValidator = require("../../utils.js/review.validator");
 const { validate } = require("express-validation");
 
 router.post("/signup", userAuth.validateUser, signup);
@@ -26,4 +28,16 @@ router.delete("/book/:id", auth_token, book.delete);
 router.get("/book", auth_token, book.find);
 router.get("/book/search", auth_token, book.findAll);
 
+router.post(
+  "/review",
+  auth_token,
+  validate(ReviewValidator.createReviewValidator),
+  review.create
+);
+router.put(
+  "/review/:id",
+  auth_token,
+  validate(ReviewValidator.updateReviewValidator),
+  review.update
+);
 module.exports = router;
---
+++ src/utils.js/review.validator.js
@@ -0,0 +1,66 @@
+const joi = require("joi");
+const { isValidObjectId } = require("../utils.js/validator");
+
+const ReviewValidation = {
+  createReviewValidator: {
+    body: joi.object({
+      reviewText: joi.string().required(),
+      stars: joi.number().max(5).min(1),
+
+      user: joi.string().custom((value, helper) => {
+        if (!isValidObjectId(value))
+          return helper.message("Please enter a valid user ID");
+        return value;
+      }, "ObjectID Validation"),
+
+      likes: joi.string().custom((value, helper) => {
+        if (!isValidObjectId(value))
+          return helper.message("Please enter a valid like ID");
+        return value;
+      }, "ObjectID Validation"),
+      book: joi
+        .string()
+        .required()
+        .custom((value, helper) => {
+          if (!isValidObjectId(value))
+            return helper.message("Please enter a valid book ID");
+          return value;
+        }, "ObjectID Validation"),
+    }),
+  },
+
+  updateReviewValidator: {
+    body: joi.object({
+      reviewText: joi.string().required(),
+      stars: joi.number().max(5).min(1),
+
+      user: joi
+        .string()
+        .required()
+        .custom((value, helper) => {
+          if (!isValidObjectId(value))
+            return helper.message("Please enter a valid user ID");
+          return value;
+        }, "ObjectID Validation"),
+
+      likes: joi
+        .string()
+        .required()
+        .custom((value, helper) => {
+          if (!isValidObjectId(value))
+            return helper.message("Please enter a valid user ID");
+          return value;
+        }, "ObjectID Validation"),
+
+      book: joi
+        .string()
+        .required()
+        .custom((value, helper) => {
+          if (!isValidObjectId(value))
+            return helper.message("Please enter a valid book ID");
+          return value;
+        }, "ObjectID Validation"),
+    }),
+  },
+};
+module.exports = ReviewValidation;
