932365b210a4ca5ce22f579bc7fbe8f7d8b82f89
chisom <chisom.ofoedu@flutterwavego.com>
2022-11-23 08:56:24 +0100

    added a disable property for version key

--- src/models/review.js
+++ src/models/review.js
@@ -1,34 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let ReviewSchema = new Schema({
-  reviewText: {
-    type: String,
-    required: true,
-  },
+let ReviewSchema = new Schema(
+  {
+    reviewText: {
+      type: String,
+      required: true,
+    },
 
-  stars: {
-    type: Number,
-    default: 5,
-    min: [1, "It must be at least 1 star"],
-    max: [5, "It must not be more than 5 stars"],
-  },
+    stars: {
+      type: Number,
+      default: 5,
+      min: [1, "It must be at least 1 star"],
+      max: [5, "It must not be more than 5 stars"],
+    },
 
-  likes: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-  },
+    likes: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
 
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    book: {
+      type: Schema.Types.ObjectId,
+      ref: "book",
+      required: true,
+    },
   },
-  book: {
-    type: Schema.Types.ObjectId,
-    ref: "book",
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = mongoose.model("review", ReviewSchema);
